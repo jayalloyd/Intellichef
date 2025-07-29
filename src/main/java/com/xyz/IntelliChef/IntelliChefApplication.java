a59b715568fe38da9eb852a6f32dc000b26eaf0c
jayarani.ys <jayaraniys1@gmail.com>
2025-07-29 21:25:12 +0530

    intial commit

---
+++ src/main/java/com/xyz/IntelliChef/IntelliChefApplication.java
@@ -0,0 +1,13 @@
+package com.xyz.IntelliChef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelliChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelliChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xyz/IntelliChef/IntelliChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.IntelliChef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelliChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
